1b6b79daf64a7e6e3818a66a0470de9756017551
AT8-dev <99163509+AT8-dev@users.noreply.github.com>
2022-02-17 11:25:24 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-
+document.getElementById("year").innerHTML = (new Date).getUTCFullYear();
+function handleClick() {
+  var l = document.getElementById('Links'); 'block' === l.style.display?l.style.display = 'none': l.style.display = 'block'
+}
